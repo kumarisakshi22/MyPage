969892d88fae78c3f653495ade657c387a128d35
kumari sakshi <kumarisakshi2210@gmail.com>
2023-04-26 13:53:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 // import logo from './logo.svg';
+// import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 
 // let name = "harry ";
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#2d2929';
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+     
+    
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Textutils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3" >
-        <TextForm heading="Enter the text to analyse" />
+        <TextForm heading="Enter the text to analyse" mode={mode} />
       </div>
     </>
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,25 +9,59 @@ export default function TextForm(props) {
         setText(newText)
         // setText("You have clicked on HandleUp Click")
     }
+    const handleLoClick = () => {
+        console.log("LowerCase was clicked " + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+        // setText("You have clicked on HandleUp Click")
+    }
+    const handleClearClick = () => {
+        console.log("clear text" + text);
+        let newText = '';
+        setText(newText)
+        // setText("You have clicked on HandleUp Click")
+    }
     const handleOnChange = (event) => {
         console.log("On Change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here2');
+    const handleCopy = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        // setText(newText)
+        // setText("You have clicked on HandleUp Click")
+    }
+    const [text, setText] = useState('');
 
     // text = "new text" // wrong way
     // setText("kljeiowj");
     // setText("new text");
     return (
-        <div>
+        <>
 
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
+            <div className="conatiner" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
 
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#2d2929' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>convert to upper case</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Conver to lower case </button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>copy text</button>
+
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>convert to upper case</button>
+            <div className="conatiner my-2" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h1 >Your text summary</h1>
+                <p>{text.split(" ").length}words and {text.length} charecters </p>
 
-        </div>
+                <p>{0.008 * text.split(" ").length} Minutes read </p>
+                <h3>Preview</h3>
+                <p>{text.length > 0 ? text : "Enter something to preview it here"}</p>
+            </div>
+
+        </>
     );
 };
